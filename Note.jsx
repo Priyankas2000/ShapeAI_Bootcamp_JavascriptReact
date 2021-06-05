a7e25befa08f78b6130a2bfb72dbdbca9a9ede86
Priyankas2000 <85400088+Priyankas2000@users.noreply.github.com>
2021-06-05 06:43:46 -0700

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>About</h1>
+      <p>
+        Spiritual guru and the founder of the Art of Living Foundation, Sri Sri
+        Ravi Shankar’s also has entered into the Ayurvedic products market and
+        the brand is known as Sri Sri Ayurveda or SSA. Sri Sri Ayurveda provides
+        medicine and Ayurvedic products in categories such as food, beverages,
+        personal care, herbal products, health and wellness. Sri Sri Ayurveda
+        products are available at franchise shops. SSA products can also be
+        purchased online.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
